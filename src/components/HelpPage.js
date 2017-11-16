cb1c5e5391ac49f257122b305d607ffadffdd227
DANIEL MICALETTI <danielmicaletti@DANIELs-MacBook-Pro.local>
2017-11-16 10:32:02 -0700

    Initial commit

---
+++ src/components/HelpPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelpPage = () => (
+	<div>
+		Help Page Component FILE
+	</div>
+);
+
+export default HelpPage;
---
+++ src/playground/redux-101.js
@@ -0,0 +1,82 @@
+import { createStore } from 'redux';
+
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+		type: 'INCREMENT',
+		incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+	type: 'DECREMENT',
+	decrementBy
+});
+
+const setCount = ({ count }) => ({
+	type: 'SET',
+	count
+});
+
+const resetCount = () => ({
+	type: 'RESET'
+});
+
+// Reducers
+// 1. Reducers are pure functions
+// 2. Never change state or action
+
+const countReducer = (state={ count: 0 }, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return {
+				count: state.count + action.incrementBy
+			}
+		case 'DECREMENT':
+			return {
+				count: state.count - action.decrementBy
+			}
+		case 'SET':
+			return {
+				count: action.count
+			}
+		case 'RESET':
+			return {
+				count: 0
+			}
+		default:
+			return state;
+	}
+};
+
+const store = createStore(countReducer);
+
+const unsubscribe = store.subscribe(() => {
+	console.log(store.getState());
+});
+
+// store.dispatch({
+// 	type: 'INCREMENT',
+// 	incrementBy: 5
+// });
+
+// store.dispatch({
+// 	type: 'INCREMENT'
+// });
+
+// store.dispatch({
+// 	type: 'DECREMENT',
+// 	decrementBy: 3
+// });
+
+store.dispatch(incrementCount({ incrementBy: 5 }));
+
+store.dispatch(incrementCount());
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({ decrementBy: 2 }));
+
+store.dispatch(resetCount());
+
+store.dispatch(setCount());
+
+store.dispatch(setCount({ count: 202 }));
